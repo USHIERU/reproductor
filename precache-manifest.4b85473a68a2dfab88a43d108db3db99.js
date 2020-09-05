544ae9e9d4748e901c78c7dcbfb7e04a0e02a5ad
USHIERU <uzielcocolan@gmail.com>
2020-09-05 15:52:22 -0500

    Updates

---
+++ precache-manifest.4b85473a68a2dfab88a43d108db3db99.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c58ce948e8357fb3e8174cf0dd03816f",
+    "url": "/reproductor/index.html"
+  },
+  {
+    "revision": "875a175b0089cfc15b92",
+    "url": "/reproductor/static/css/main.efe9f3ed.chunk.css"
+  },
+  {
+    "revision": "313e18589b30dc88e531",
+    "url": "/reproductor/static/js/2.ea971663.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reproductor/static/js/2.ea971663.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "875a175b0089cfc15b92",
+    "url": "/reproductor/static/js/main.e139a05f.chunk.js"
+  },
+  {
+    "revision": "398f269db73d6a548a44",
+    "url": "/reproductor/static/js/runtime-main.c77f4b0a.js"
+  },
+  {
+    "revision": "aee57c92e5ad28645c97897129da585e",
+    "url": "/reproductor/static/media/arroz.aee57c92.mp4"
+  },
+  {
+    "revision": "ba8ecc712b8e50d71a8c2345a1e1630c",
+    "url": "/reproductor/static/media/arroz.ba8ecc71.png"
+  },
+  {
+    "revision": "6e22b2080236ceee2634fc3f9b57a45d",
+    "url": "/reproductor/static/media/flor.6e22b208.mp4"
+  },
+  {
+    "revision": "ea96faab33084b7c953b85877f69f72d",
+    "url": "/reproductor/static/media/flor.ea96faab.png"
+  },
+  {
+    "revision": "6709b3d096306953953b36629804d391",
+    "url": "/reproductor/static/media/lighthouse.6709b3d0.mp4"
+  },
+  {
+    "revision": "68e8bcbb53169b6c2870ee6ee69048d1",
+    "url": "/reproductor/static/media/lighthouse.68e8bcbb.png"
+  },
+  {
+    "revision": "7fb8ae6c332610f211a7127550801f60",
+    "url": "/reproductor/static/media/playa.7fb8ae6c.mp4"
+  },
+  {
+    "revision": "8f89e61a189dfa8829e8017be37fb828",
+    "url": "/reproductor/static/media/playa.8f89e61a.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/USHIERU/reproductor/precache-manifest.c27b1fa41b6ecad3ff446a227fdd3085.js"
+  "/reproductor/precache-manifest.4b85473a68a2dfab88a43d108db3db99.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/USHIERU/reproductor/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/reproductor/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
